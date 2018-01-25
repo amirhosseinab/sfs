d8d339dcce3c878d0e2690c0d6824c7d8fc9f2b9
Amir Hossein Abdolkhalegh <ah.abdolkhalegh@gmail.com>
2018-01-25 22:26:49 +0330

    implement serving static files

---
+++ static-file-server.go
@@ -0,0 +1,44 @@
+package sfs
+
+import (
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+)
+
+type StaticFileServer struct {
+	Root            http.Dir
+	FallbackHandler http.Handler
+}
+
+func NewStaticFileServer(root http.Dir, fallback http.HandlerFunc) (*StaticFileServer, error) {
+	fs := &StaticFileServer{
+		Root:            root,
+		FallbackHandler: fallback,
+	}
+
+	return fs, nil
+}
+
+func (fs StaticFileServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	p := r.URL.Path
+	if !strings.HasPrefix(p, "/") {
+		p = "/" + p
+		r.URL.Path = p
+	}
+	p = path.Clean(p)
+
+	name := path.Join(string(fs.Root), filepath.FromSlash(p))
+
+	f, err := os.Open(name)
+	if err != nil {
+		if os.IsNotExist(err) {
+			fs.FallbackHandler.ServeHTTP(w, r)
+			return
+		}
+	}
+	defer f.Close()
+	http.ServeFile(w, r, name)
+}
